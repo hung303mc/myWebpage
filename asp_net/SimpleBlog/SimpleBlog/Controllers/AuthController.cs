edf426b1f4afac04c50414bdeb8b80456572440a
hung303mc <hung216mc@gmail.com>
2019-03-22 16:29:07 +0700

    Add view model into our simple blog

--- asp_net/SimpleBlog/SimpleBlog/Controllers/AuthController.cs
+++ asp_net/SimpleBlog/SimpleBlog/Controllers/AuthController.cs
@@ -3,14 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SimpleBlog.ViewModels;
 
 namespace SimpleBlog.Controllers
 {
     public class AuthController : Controller
     {
+
         public ActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(AuthLogin form)
+        {
+            if (!ModelState.IsValid)
+                return View(form);
+
+            if(form.Username != "Hung303mc")
+            {
+                ModelState.AddModelError("Username", "Your username is not cool :)");
+                return View(form);
+            }
+            // Pass 'form' into view since it need info from our Models
+            return Content("The form is valid");
         }
     }
 }
