561328946606660fbbe25f416fbb17ce57459c53
hung303mc <hung216mc@gmail.com>
2019-03-23 15:37:23 +0700

    using bundle to merge a lot of js script/css file into one

---
+++ asp_net/SimpleBlog/SimpleBlog/App_Start/BundleConfig.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace SimpleBlog
+{
+    public class BundleConfig
+    {
+        public static void RegisterBunldes(BundleCollection bundles)
+        {
+            // ~/admin/styles: Virtual path <=> name
+            bundles.Add(new StyleBundle("~/admin/styles")
+                .Include("~/content/styles/bootstrap.css")
+                .Include("~/content/styles/admin.css"));
+
+            // ~/styles: Virtual path <=> name
+            bundles.Add(new StyleBundle("~/styles")
+                .Include("~/content/styles/bootstrap.css")
+                .Include("~/content/styles/site.css"));
+
+            bundles.Add(new ScriptBundle("~/admin/scripts")
+                .Include("~/scripts/jquery-3.3.1.js")
+                .Include("~/scripts/jquery.validate.js")
+                .Include("~/scripts/jquery.validate.unobtrusive.js")
+                .Include("~/scripts/bootstrap.js"));
+
+            bundles.Add(new ScriptBundle("~/scripts")
+                .Include("~/scripts/jquery-3.3.1.js")
+                .Include("~/scripts/jquery.validate.js")
+                .Include("~/scripts/jquery.validate.unobtrusive.js")
+                .Include("~/scripts/bootstrap.js"));
+        }
+    }
+}
--- asp_net/SimpleBlog/SimpleBlog/Global.asax.cs
+++ asp_net/SimpleBlog/SimpleBlog/Global.asax.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using System.Web.Optimization;
+
 
 namespace SimpleBlog
 {
@@ -13,6 +15,7 @@ namespace SimpleBlog
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBunldes(BundleTable.Bundles);
         }
     }
 }
